4655454bec1415fd5aa4ae3de2f6bd7ad5e6d6b8
gamza <71205928+gusqls0525@users.noreply.github.com>
2025-03-18 21:10:13 +0900

    Update VideoRecoding.py
    1. 흑백화면이 b키를 누르면 실행되도록 변경.

--- VideoRecoding.py
+++ VideoRecoding.py
@@ -7,6 +7,7 @@ videoWidth = int(video.get(3))
 videoHeight = int(video.get(4))
 recodingMode = False
 nowRecoding = False
+blackMode = False
 number = 1
 fps = 30
 wait_msec = int(1 / fps * 1000)
@@ -18,11 +19,17 @@ if video.isOpened():
         valid, img = video.read()
         if not valid:
             break
-               
+
+        #흑백모드 적용
+        if blackMode:
+            img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
+            img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
         # 녹화된 프레임 따로, 출력용 프레임 따로
         displayImg = img.copy()
         recodingImg = img.copy()
 
+        
         # 녹화모드 빨간테두리
         if nowRecoding:
             cv.rectangle(displayImg, (3,3), (videoWidth - 3, videoHeight - 3), (0,0,225), 3)
@@ -44,11 +51,12 @@ if video.isOpened():
         
         key = cv.waitKey(wait_msec)
            
-        
         # 스페이스 바로 모드 전환
         if key == ord(' '):
             recodingMode = not recodingMode
-            
+        # 흑백모드
+        if key == ord('b'):
+            blackMode = not blackMode
         # R키에 녹화버튼 할당
         if key == ord('r') and recodingMode:
             if nowRecoding: #녹화중인거 멈추고 저장
@@ -58,6 +66,7 @@ if video.isOpened():
             else: #녹화시작
                 recodedVideo = cv.VideoWriter(f"CAcctv{number}.mp4", codec, fps, (videoWidth, videoHeight))
                 nowRecoding = True
+        
         # esc누르면 종료        
         if key == 27:
             break
